945a457d1f6244151e13de71931ca691192c655a
Ian McEwen <mian@cyverse.org>
2022-03-24 17:15:09 -0700

    Use the right service name

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func jaegerTracerProvider(url string) (*tracesdk.TracerProvider, error) {
 		tracesdk.WithBatcher(exp),
 		tracesdk.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
-			semconv.ServiceNameKey.String("data-usage-api"),
+			semconv.ServiceNameKey.String("user-info"),
 		)),
 	)
 
